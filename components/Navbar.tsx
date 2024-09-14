3158835f9ce1a46e3470939065a51b4f7d799a1f
enatanslice <eliran.natan.87@gmail.com>
2024-09-14 12:37:04 +0300

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import React from 'react';
-import DarkModeToggle from './DarkModeToggle'
 
 export default function Navbar() {
 
